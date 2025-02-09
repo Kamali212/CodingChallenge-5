c1b2aa4daade675b1f5c9c32da28a073448e1627
Kamali Hicks <kamalikh3@gmail.com>
2025-02-08 20:18:18 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,5 @@
+// Task 1: Object Profiles
+let customer = {Name: "Bob Belcher", Age: 45, Email: "BobBelcher@gmail.com"}; // Declare an object customer with properties
+console.log(`Name: ${customerName}`); // Log customerName property to console
+console.log(`Age: ${customerAge}`); // Log customerAge property to console
+console.log(`Email: ${customerEmail}`); // Log customerEmail property to console
