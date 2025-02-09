b878d00b2370cbdf4eaa768f4b9ebd5d084c9243
Kamali Hicks <kamalikh3@gmail.com>
2025-02-08 20:57:54 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -1,5 +1,17 @@
 // Task 1: Object Profiles
 let customer = {Name: "Bob Belcher", Age: 45, Email: "BobBelcher@gmail.com"}; // Declare an object customer with properties
-console.log(`Name: ${customerName}`); // Log customerName property to console
-console.log(`Age: ${customerAge}`); // Log customerAge property to console
-console.log(`Email: ${customerEmail}`); // Log customerEmail property to console
+console.log(`Name: ${customer.Name}`); // Log customerName property to console
+console.log(`Age: ${customer.Age}`); // Log customerAge property to console
+console.log(`Email: ${customer.Email}`); // Log customerEmail property to console
+
+// Task 2: Object Methods
+const order = {orderID: 23456, totalAmount: 200, status: "Processing", // Declare order properties
+displayOrder() { // Log details of order when called
+    console.log(`Order ID: ${this.orderID}`); // References orderID
+    console.log(`Order Total: ${this.totalAmount}`); // References totalAmount
+    console.log(`Order Status: ${this.status}`); // References status
+}
+};
+order.displayOrder(); // Logs order details to console
+
+
